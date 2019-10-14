9da3c6ef55aea4095e31735c40636380df57da56
duchometson <ducho.metson@hotmail.com>
2019-10-14 10:51:41 -0300

    VERSAO FINAL AGORA VAI

--- Codigos/Solucao.h
+++ Codigos/Solucao.h
@@ -1,3 +1,18 @@
+
+/************************************
+*       Estrutura Solucao
+*   -> Implementada para armazenar em memória as soluções do problema
+* 
+*   Campos:
+*       1. *cidades    =>   Vetor de cidades que indicam o trajeto
+*       2. distancia   =>   soma das distancias do trajeto dessa solução
+*       3. tempo       =>   Valor em segundos para cronometrar o tempo que 
+*                             demorou para alcançar essa solucao
+* 
+*   Estrutura escolhida para o armazenamento das distancias foi uma matriz.
+* 
+*************************************/
+
 struct solucao {
     int *cidades;
     int distancia;
@@ -5,5 +20,20 @@ struct solucao {
 }; typedef struct solucao Solucao;
 
 
-// Funções para solução.
+/* 
+* criarSolucao
+*  Responsável por alocar uma solução em memória
+* 
+*  método:
+*       Recebe vetor de cidades, valor da distancia e tamanho do vetor de cidades
+*       para que possa retornar um vetor de ponteiros de soluções
+* 
+*  Parametros:
+*       int *cidade   -> Vetor de cidades  
+*       int distancia -> inteiro que representa a distancia percorrida nesse
+*                        trajeto
+*       int dimension -> inteiro que representa tamanho do vetor cidade
+*  
+*/
+
 Solucao **criarSolucao( int *cidades, int distancia, int dimension );
